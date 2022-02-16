29025f4d8c172ca321392535fbe874f8bdd40fb5
Viktar Rabtsau <95769479+Reinele@users.noreply.github.com>
2022-02-16 20:15:10 +0300

    Add files via upload

---
+++ 2/src/by/training/simplestapp/main/2.java
@@ -0,0 +1,18 @@
+public class Main {
+
+	public static void main(String[] args) {
+		    double b;
+		    double a;
+		    double c; 
+	        double k;
+	        
+	        b = 1;
+	        a = 2;
+	        c = 3;
+	        k = Math.sqrt(b * b + 4 * a * c);
+	        System.out.println(b + k / 2 * a - a * a * a * c + b / 2);
+	        
+	       
+	         }
+	}
+
